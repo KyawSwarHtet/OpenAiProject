373c56d42313a307abf669663cbf1ae4068921a4
KyawSwarHtet <kyawswarhtet101094@gmail.com>
2023-01-20 00:19:21 +0630

    render route add

--- client/script.js
+++ client/script.js
@@ -85,8 +85,8 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's responser
-  // const response = await fetch("https://codex-im0y.onrender.com/", {
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://kshopenai.onrender.com/", {
+    // const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
